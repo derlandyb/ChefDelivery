e09cf7d11d3297c8d6023e774a77e9e715d5c64f
derlandyb <derlandy.belchior@gmail.com>
2024-06-06 13:02:57 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Derlandy Belchior on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
